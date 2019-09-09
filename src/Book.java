d8a2c5e4880bd6207f884ce5387d318a7736b7cd
PaulZhn <tianxin8431@hotmail.com>
2019-09-09 18:23:56 +0800

    assignment 1

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public Book(String name, Author author, double price) {
+        
+    }
+}
---
+++ src/TestAuthor.java
@@ -0,0 +1,7 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck);
+        System.out.println(ahTeck.getEmail() + " " + ahTeck.getName() + " " + ahTeck.getGender());
+    }
+}
